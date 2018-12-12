02419813a00fa397d2ab2e705ca869b484bab091
Ilya Amelchenkov <ilya.amelchenkov@gmail.com>
2018-12-13 00:26:01 +0300

    Initial Commit

---
+++ SnapshotAppstore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotAppstore
+//
+//  Created by Ilya Amelchenkov on 13/12/2018.
+//  Copyright © 2018 Aviasales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
